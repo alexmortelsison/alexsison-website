ffd59c8f19b6f09df5d1b339964ca5f4ec4a144a
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-11-06 20:05:27 +0900

    updated

--- app/components/CardDemoThree.tsx
+++ app/components/CardDemoThree.tsx
@@ -11,7 +11,7 @@ export function CardDemoOneThree() {
           "bg-[url(https://images.unsplash.com/photo-1560472355-a3b4bcfe790c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover"
         )}
       >
-        <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
+        <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-65"></div>
         <div className="flex flex-row items-center space-x-4 z-10">
           <Image
             height="100"
